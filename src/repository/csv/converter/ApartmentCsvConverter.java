2c9e203316bfca54d9c3982b1c1645bb168a28fe
vucaj <vucaj1998@outlook.com>
2020-07-30 16:53:02 +0200

    CSVConverters

--- src/repository/csv/converter/ApartmentCsvConverter.java
+++ src/repository/csv/converter/ApartmentCsvConverter.java
@@ -6,9 +6,11 @@
 
 package repository.csv.converter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
 
+import beans.Address;
 import beans.Amenity;
 import beans.Apartment;
 import beans.ApartmentType;
@@ -33,8 +35,12 @@ public class ApartmentCsvConverter implements ICsvConverter<Apartment> {
       joiner.add(String.valueOf(entity.getCheckInHour()));
       joiner.add(String.valueOf(entity.getCheckOutHour()));
       joiner.add(String.valueOf(entity.getApartmentType()));
-      joiner.add(String.valueOf(entity.getLocation()));
-      joiner.add(String.valueOf(entity.getHost().getId()));
+      //TODO: proveriti
+      joiner.add(String.valueOf(entity.getLocation().getLatitude()));
+      joiner.add(String.valueOf(entity.getLocation().getLongitude()));
+      //joiner.add(String.valueOf(entity.getLocation().getAddress()));
+      
+      joiner.add(String.valueOf(entity.getHost() == null? "" : entity.getHost().getId()));
       
       joiner.add(getPictureListString(entity.getPictures()));
       joiner.add(getAmenityListString(entity.getAmenities()));
@@ -44,11 +50,85 @@ public class ApartmentCsvConverter implements ICsvConverter<Apartment> {
       return joiner.toString();
    }
    
+   /*
+    *  private int numberOfRooms;
+   private int numberOfGuests;
+   private long id;
+   private double pricePerNight;
+   private boolean deleted;
+   private boolean active;
+   private int checkInHour = 14;
+   private int checkOutHour = 10;
+   
+   private ApartmentType apartmentType;
+   private Location location;
+   private User host;
+   private List<Picture> pictures;
+   private List<Amenity> amenities;
+   private List<Comment> comments;
+    * 
+    */
+   
    public Apartment fromCsv(String entityCsv) {
-      // TODO: implement
-      return null;
+      String[] tokens = entityCsv.split(delimiter);
+      
+      int numberOfRooms = Integer.valueOf(tokens[0]);
+      int numberOfGuests = Integer.valueOf(tokens[1]);
+      long id = Long.valueOf(tokens[2]);
+      double pricePerNight = Double.valueOf(tokens[3]);
+      boolean deleted = Boolean.valueOf(tokens[4]);
+      boolean active = Boolean.valueOf(tokens[5]);
+      int checkInHour = Integer.valueOf(tokens[6]);
+      int checkOutHour = Integer.valueOf(tokens[7]);
+      ApartmentType apartmentType = ApartmentType.valueOf(tokens[8]);
+      //TODO: proveriti 
+	  Location location = new Location(Double.valueOf(tokens[9]), Double.valueOf(tokens[10]), new Address());
+	  User host = new User(Long.valueOf(tokens[11]));
+	  
+	  List<Picture> pictures = getPictureList(tokens[12]);
+	  List<Amenity> amenities = getAmenityList(tokens[13]);
+	  List<Comment> comments = getCommentList(tokens[14]);
+	  
+	  Apartment retVal = new Apartment(numberOfRooms, numberOfGuests, pricePerNight, deleted, active, apartmentType, location, host, pictures, amenities, comments);
+	  
+      return retVal;
    }
 
+   private List<Picture> getPictureList(String pictureString)
+   {
+	   List<Picture> retVal = new ArrayList<Picture>();
+	   
+	   for(String s: pictureString.split(listDelimiter))
+	   {
+		   retVal.add(new Picture(s));
+	   }
+	   
+	   return retVal;
+   }
+   
+   private List<Amenity> getAmenityList(String amenityString)
+   {
+	   List<Amenity> retVal = new ArrayList<Amenity>();
+	   
+	   for(String s: amenityString.split(listDelimiter))
+	   {
+		   retVal.add(new Amenity(Long.valueOf(s)));
+	   }
+	   
+	   return retVal;
+   }
+   
+   private List<Comment> getCommentList(String commentString)
+   {
+	   List<Comment> retVal = new ArrayList<Comment>();
+	   
+	   for(String s: commentString.split(listDelimiter))
+	   {
+		   retVal.add(new Comment(Long.valueOf(s)));
+	   }
+	   
+	   return retVal;
+   }
    
    private String getPictureListString(List<Picture> pictures)
    {
